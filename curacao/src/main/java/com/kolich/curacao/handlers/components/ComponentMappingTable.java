20d71f3df3940e04ffa4608e6845055772f3368a
Mark Kolich <social@kolich.com>
2013-11-30 22:23:12 -0800

    super minor comment cleanup

--- curacao/src/main/java/com/kolich/curacao/handlers/components/ComponentMappingTable.java
+++ curacao/src/main/java/com/kolich/curacao/handlers/components/ComponentMappingTable.java
@@ -78,7 +78,11 @@ public final class ComponentMappingTable {
 		}
 		bootSwitch_ = new AtomicBoolean(false);
 	}
-	
+
+    // This makes use of the "Initialization-on-demand holder idiom" which is
+    // discussed in detail here:
+    // http://en.wikipedia.org/wiki/Initialization-on-demand_holder_idiom
+    // As such, this is totally thread safe and performant.
 	private static class LazyHolder {
 		private static final ComponentMappingTable instance__ =
 			new ComponentMappingTable();
